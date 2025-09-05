4e842282d86d89252c60ebbe4cd22cd8f29e0e4f
mattobial <mattobial@gmail.com>
2025-09-06 00:44:26 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,10 @@
-from setuptools import setup
-setup()
+from setuptools import setup, find_packages
+
+setup(
+    name="company_theme",
+    version="0.0.1",
+    description="Custom ERPNext desk sidebar theme",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+)
